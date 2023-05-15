3ed91781f85d752af1886bf38aeb1df554cf555b
Natasha Malhan <natashamalhan@natashas-air.lan>
2023-05-15 13:05:20 -0700

    first commit

---
+++ controllers/api/veggieRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router()
+const {Veggie} = require("../../models")
+
+router.get("/", async (req, res) =>{
+try {
+    const veggie = await Veggie.findAll()
+    res.status(200).json(veggie)
+
+} catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+}
+})
+
+module.exports = router
---
+++ models/Veggie.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require("bcrypt")
+
+class Veggie extends Model {}
+
+Veggie.init(
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique:false
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+  }
+);
+
+module.exports = Veggie;
